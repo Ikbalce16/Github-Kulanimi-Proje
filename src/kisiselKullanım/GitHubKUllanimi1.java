501e82fde97667c935dda5dbe748664e4eac27d8
ikbal c <ikbalce16@gmail.com>
2022-07-19 21:23:53 +0300

    ilk degisiklik

--- src/kisiselKullanım/GitHubKUllanimi1.java
+++ src/kisiselKullanım/GitHubKUllanimi1.java
@@ -3,5 +3,7 @@ package kisiselKullanım;
 public class GitHubKUllanimi1 {
     public static void main(String[] args) {
         System.out.println("Hello world");
+
+        System.out.println("that's it");
     }
 }
